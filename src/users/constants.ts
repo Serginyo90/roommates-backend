505bee4d490a9dcd150397e7c29e4f1fa4dca7c1
Sergey Cherbadzhy <s.cherbadzhy@gmail.com>
2020-12-08 14:57:09 +0300

    feat(local.strategy.ts): add error Password is not correct

--- src/users/constants.ts
+++ src/users/constants.ts
@@ -1,4 +1,5 @@
 export const UserNotFound = 'User not found';
+export const PasswordIsNotCorrect = 'Password is not correct';
 export const InvalidToken = 'Invalid token';
 export const UserAlreadyExists = 'User already exists';
 export const UserAlreadyRegistered = 'User already registered';
